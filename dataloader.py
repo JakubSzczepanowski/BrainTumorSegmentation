12480bbe48da23abb83f56aa445f748a820023af
Jakub Szczepanowski <jakub.szczepanowski.rad@gmail.com>
2024-02-28 12:49:44 +0100

    Tidy up for colab

--- dataloader.py
+++ dataloader.py
@@ -9,20 +9,11 @@ import cv2
 from sklearn.model_selection import train_test_split
 from functools import lru_cache
 
-# folder treningowy zawiera HGG i LGG. Należy stworzyć generator łączący oba zbiory w jeden potasowany w losowej kolejności. Każdy element to oddzielny mózg. Każdy mózg to (240, 240, 155)
-# jedna próbka będzie jednym przekrojem w kilku kanałach, a więc będzie tensorem o wymiarach (240, 240, channels). Takich próbek będzie tyle ile we wsadzie
-# należy opracować metodę, aby pojedynczy wsad był jak najbardziej reprezentatywny dla procesu optymalizacji. Nie może być tak, że cały wsad będzie składał się z początkowych slice'ów pojedynczego mózgu
-# batch_size/brain_slices to ile klatek pobieramy z każdego slicea, 155/batch_size = wynik to ile wsadów na jeden mózg, 155 - wynik * batch_size = reszta,
-# reszta/2 = offset, [offset-1:155-offset]
-# przykład: batch_size = 32, wynik = 155/32 = 4 = steps, 4*32 = 128, 155-128 = 27, 27/2=13, [12:142], brain_slices = 8, 32/8=4 = sample_size, 155/8 = 19 = slice_size
-# for step in range(steps): for slice in range(brain_slices): low = slice*slice_size (if slice != 0: -1) + step*sample_size 
-# [low + offset : low + sample_size - offset]
-
 BRAIN_FRAMES = 155
 IMAGE_SIZE = 128
 CHANNELS = 4
 X_DTYPE = np.float32
-Y_DTYPE = np.float32
+Y_DTYPE = np.uint8
 LABEL_MAPPING_PATTERN = {0: 0, 2: 1, 4: 2, 1: 3}
 
 @dataclass
@@ -60,62 +51,10 @@ def load_dataset_paths(labeled_path: str, unlabeled_path: str, validation_size_f
 def extend_path_from_last_part(path: str) -> str:
     return os.path.join(path, os.path.basename(path))
 
-class FrameCutter:
-
-    def __init__(self, frame_radius: int = 5):
-        middle = BRAIN_FRAMES/2
-        offset = frame_radius/2
-        self.low =  int(middle - offset)
-        self.high = int(middle + offset)
-        self.w_low_index, self.w_high_index, self.h_low_index, self.h_high_index = IMAGE_SIZE - 1, 0, IMAGE_SIZE - 1, 0
-
-    def fit(self, paths: list[str]):
-
-        for path in paths:
-            extended = extend_path_from_last_part(path)
-            clip = nib.load(f'{extended}_t1.nii').get_fdata(dtype=X_DTYPE)[:, :, self.low:self.high]
-            for i in range(clip.shape[-1]):
-                resized = cv2.resize(clip[:, :, i], (IMAGE_SIZE, IMAGE_SIZE))
-                h_low_index, h_high_index, w_low_index, w_high_index = self._find_limit_values(resized)
-
-                self.h_low_index = min(self.h_low_index, h_low_index)
-                self.h_high_index = max(self.h_high_index, h_high_index)
-
-                self.w_low_index = min(self.w_low_index, w_low_index)
-                self.w_high_index = max(self.w_high_index, w_high_index)
-
-
-    def transform(self, array: np.ndarray) -> np.ndarray:
-        if self.w_low_index == IMAGE_SIZE - 1 and self.w_high_index == 0 and self.h_low_index == IMAGE_SIZE - 1 and self.h_high_index == 0:
-            raise FrameCutter.NotFittedError()
-        
-        return array[self.h_low_index:self.h_high_index, self.w_low_index:self.w_high_index]
-
-    def _find_limit_values(self, array: np.ndarray) -> tuple[int, int, int, int]:
-        
-        def get_through_axis(array, mask, axis):
-            low_indexes = mask.argmax(axis=axis)
-            low_index = low_indexes[low_indexes.nonzero()].min()
-            high_indexes = np.flip(mask, axis=axis).argmax(axis=axis)
-            high_index = (array.shape[0] - high_indexes[high_indexes.nonzero()] - 1).max()
-
-            return low_index, high_index
-
-        mask = array != 0
-        h_low_index, h_high_index = get_through_axis(array, mask, axis=0)
-        w_low_index, w_high_index = get_through_axis(array, mask, axis=1)
-
-        return h_low_index, h_high_index, w_low_index, w_high_index
-        
-    
-    class NotFittedError(Exception):
-
-        def __init__(self):
-            super('Frame cutter was not fitted! Use .fit method or fill index fields manually')
 
 class DataGenerator(tf.keras.utils.Sequence):
 
-    def __init__(self, dataset_paths: list[str], max_value: int, hgg_size: int, lgg_size: int, batch_size: int = 32, brain_slices: int = 8, X_dtype = X_DTYPE, Y_dtype = Y_DTYPE, bootstrap: bool = True):
+    def __init__(self, dataset_paths: list[str], max_value: int, batch_size: int = 32, brain_slices: int = 8, X_dtype = X_DTYPE, Y_dtype = Y_DTYPE, bootstrap: bool = True, hgg_size: int = None, lgg_size: int = None):
         self.dataset_paths = [extend_path_from_last_part(path).decode('ASCII') for path in dataset_paths]
         self.brain_slices = brain_slices
         self.batch_size = batch_size
@@ -185,15 +124,8 @@ class DataGenerator(tf.keras.utils.Sequence):
         step = idx % self.batches_per_brain
 
         if step == 0:
-            extended = extend_path_from_last_part(self.dataset_paths[idx // self.batches_per_brain])
-            t1 = nib.load(f'{extended}_t1.nii').get_fdata(dtype=self.X_dtype)
-            t1ce = nib.load(f'{extended}_t1ce.nii').get_fdata(dtype=self.X_dtype)
-            t2 = nib.load(f'{extended}_t2.nii').get_fdata(dtype=self.X_dtype)
-            flair = nib.load(f'{extended}_flair.nii').get_fdata(dtype=self.X_dtype)
-            seg = nib.load(f'{extended}_seg.nii').get_fdata(dtype=self.X_dtype)
 
-
-            self.cur_brain = Brain(t1, t1ce, t2, flair, seg)
+            self.cur_brain = self._load_brain(idx // self.batches_per_brain)
 
         batch_X = np.zeros((self.batch_size, IMAGE_SIZE, IMAGE_SIZE, CHANNELS), dtype=self.X_dtype)
         batch_Y = np.zeros((self.batch_size, IMAGE_SIZE, IMAGE_SIZE), dtype=self.Y_dtype)
@@ -203,11 +135,11 @@ class DataGenerator(tf.keras.utils.Sequence):
         for slice in range(self.brain_slices):
             low = max(slice * self.slice_size + step * self.sample_size + self.offset - 1, 0)
             high = min(low + self.sample_size, ceil)
-            t1_cut = self.cur_brain.t1[:, :, low : high]
-            t1ce_cut = self.cur_brain.t1ce[:, :, low : high]
-            t2_cut = self.cur_brain.t2[:, :, low : high]
-            flair_cut = self.cur_brain.flair[:, :, low : high]
-            seg_cut = self.cur_brain.seg[:, :, low : high]
+            t1_cut = self.cur_brain.t1.get_fdata(dtype=self.X_dtype)[:, :, low : high]
+            t1ce_cut = self.cur_brain.t1ce.get_fdata(dtype=self.X_dtype)[:, :, low : high]
+            t2_cut = self.cur_brain.t2.get_fdata(dtype=self.X_dtype)[:, :, low : high]
+            flair_cut = self.cur_brain.flair.get_fdata(dtype=self.X_dtype)[:, :, low : high]
+            seg_cut = self.cur_brain.seg.get_fdata(dtype=self.X_dtype)[:, :, low : high]
             X_cuts = (t1_cut, t1ce_cut, t2_cut, flair_cut)
 
             for i in range(self.sample_size):
@@ -218,7 +150,7 @@ class DataGenerator(tf.keras.utils.Sequence):
                 batch_Y[batch_index, :, :] = cv2.resize(seg_cut[:, :, i], (IMAGE_SIZE, IMAGE_SIZE))
                 batch_index += 1
 
-        return batch_X/self.max_value, tf.one_hot(batch_Y)
+        return batch_X/self.max_value, tf.one_hot(batch_Y, 4, dtype=self.Y_dtype)
     
     def __enter__(self):
         return self
@@ -226,8 +158,8 @@ class DataGenerator(tf.keras.utils.Sequence):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._load_brain.cache_clear()
 
-def build_data_generator(dataset_paths: list[str], max_value: int, hgg_size: int, lgg_size: int, batch_size: int = 32, brain_slices: int = 8, X_dtype = X_DTYPE, Y_dtype = Y_DTYPE, bootstrap: bool = True) -> Iterator[tuple[np.ndarray, tf.Tensor]]:
-    with DataGenerator(dataset_paths, max_value, hgg_size, lgg_size, batch_size, brain_slices, X_dtype, Y_dtype, bootstrap) as generator:
+def build_data_generator(dataset_paths: list[str], max_value: int, batch_size: int = 32, brain_slices: int = 8, X_dtype = X_DTYPE, Y_dtype = Y_DTYPE, bootstrap: bool = True, hgg_size: int = None, lgg_size: int = None) -> Iterator[tuple[np.ndarray, tf.Tensor]]:
+    with DataGenerator(dataset_paths, max_value, batch_size, brain_slices, X_dtype, Y_dtype, bootstrap, hgg_size, lgg_size) as generator:
 
         for i in range(generator.len):
             yield generator.__getitem__(i)
