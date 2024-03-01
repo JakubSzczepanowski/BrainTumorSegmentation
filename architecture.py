3ce26ebce117081a6e8e04efffa94037597412db
Jakub Szczepanowski <jakub.szczepanowski.rad@gmail.com>
2024-03-01 16:00:12 +0100

    v.1.1

--- architecture.py
+++ architecture.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import keras_cv
 
 class InceptionModule(tf.keras.layers.Layer):
 
@@ -40,29 +41,29 @@ class InceptionModule(tf.keras.layers.Layer):
 
         return tf.keras.layers.Concatenate()([result1, result2, result3, result4])
     
-def build_conv_cascade(filters: int, dropout: float, activation: str, kernel_initializer: str) -> tf.keras.Sequential:
+def build_conv_cascade(filters: int, drop_proba: float, drop_size: int, activation: str, kernel_initializer: str) -> tf.keras.Sequential:
 
     return tf.keras.Sequential([
         tf.keras.layers.DepthwiseConv2D(3, 1, 'same', filters // 4, activation=activation, kernel_initializer=kernel_initializer),
-        tf.keras.layers.Dropout(dropout),
+        keras_cv.layers.DropBlock2D(drop_proba, drop_size),
         tf.keras.layers.Conv2D(filters, 3, 1, 'same', activation=activation, kernel_initializer=kernel_initializer)
     ])
     
-def build_reduction_sequential(filters: int, dropout: float, activation: str, kernel_initializer: str, prev_node: tf.keras.layers.Layer) -> tuple[tf.keras.Sequential, InceptionModule]:
+def build_reduction_sequential(filters: int, drop_proba: float, drop_size: int, activation: str, kernel_initializer: str, prev_node: tf.keras.layers.Layer) -> tuple[tf.keras.Sequential, InceptionModule]:
 
-    seq = build_conv_cascade(filters, dropout, activation, kernel_initializer)(prev_node)
+    seq = build_conv_cascade(filters, drop_proba, drop_size, activation, kernel_initializer)(prev_node)
     inception = InceptionModule(filters, 2, activation, kernel_initializer)(seq)
 
     return seq, inception
 
-def build_expansion_sequential(filters: int, dropout: float, activation: str, kernel_initializer: str, prev_node: tf.keras.layers.Layer, concat_with: tf.keras.Sequential) -> tf.keras.Sequential:
+def build_expansion_sequential(filters: int, drop_proba: float, drop_size: int, activation: str, kernel_initializer: str, prev_node: tf.keras.layers.Layer, concat_with: tf.keras.Sequential) -> tf.keras.Sequential:
 
     e = tf.keras.layers.Conv2DTranspose(filters, 2, 2, 'same', activation=activation, kernel_initializer=kernel_initializer)(prev_node)
     e = tf.keras.layers.Concatenate()([e, concat_with])
 
     return tf.keras.Sequential([
         tf.keras.layers.Conv2D(filters, 3, 1, 'same', activation=activation, kernel_initializer=kernel_initializer),
-        tf.keras.layers.Dropout(dropout),
+        keras_cv.layers.DropBlock2D(drop_proba, drop_size),
         InceptionModule(filters, 1, activation, kernel_initializer)
     ])(e)
 
@@ -70,23 +71,23 @@ def build_model(input_shape: tuple[int, int, int], num_classes: int, activation:
 
     input_layer = tf.keras.layers.Input(input_shape)
 
-    r1, i1 = build_reduction_sequential(16, 0.1, activation, kernel_initializer, input_layer)
+    r1, i1 = build_reduction_sequential(16, 0, 1, activation, kernel_initializer, input_layer)
 
-    r2, i2 = build_reduction_sequential(32, 0.1, activation, kernel_initializer, i1)
+    r2, i2 = build_reduction_sequential(32, 0, 1, activation, kernel_initializer, i1)
 
-    r3, i2 = build_reduction_sequential(64, 0.2, activation, kernel_initializer, i2)
+    r3, i2 = build_reduction_sequential(64, 0.1, 16, activation, kernel_initializer, i2)
 
-    r4, i3 = build_reduction_sequential(128, 0.2, activation, kernel_initializer, i2)
+    r4, i3 = build_reduction_sequential(128, 0.15, 8, activation, kernel_initializer, i2)
 
-    r5 = build_conv_cascade(256, 0.3, activation, kernel_initializer)(i3)
+    r5 = build_conv_cascade(256, 0.2, 4, activation, kernel_initializer)(i3)
 
-    e = build_expansion_sequential(128, 0.2, activation, kernel_initializer, r5, r4)
+    e = build_expansion_sequential(128, 0.15, 8, activation, kernel_initializer, r5, r4)
 
-    e = build_expansion_sequential(64, 0.2, activation, kernel_initializer, e, r3)
+    e = build_expansion_sequential(64, 0.1, 16, activation, kernel_initializer, e, r3)
 
-    e = build_expansion_sequential(32, 0.2, activation, kernel_initializer, e, r2)
+    e = build_expansion_sequential(32, 0, 1, activation, kernel_initializer, e, r2)
 
-    e = build_expansion_sequential(16, 0.1, activation, kernel_initializer, e, r1)
+    e = build_expansion_sequential(16, 0, 1, activation, kernel_initializer, e, r1)
 
     output = tf.keras.layers.Conv2D(num_classes, 1, 1, 'same', activation='softmax')(e)
 
